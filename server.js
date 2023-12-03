d8ab4494f9bb4e090d62654c75f2480283ea13e5
AanchalBasra <abasra3@myseneca.ca>
2023-12-03 14:40:44 -0500

    fgh

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /********************************************************************************
-*  WEB322 – Assignment 05
+*  WEB322 – Assignment 06
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
@@ -15,6 +15,8 @@
 ********************************************************************************/
 
 
+const authData = require("./modules/auth-service.js");
+const clientSessions = require("client-sessions");
 
 const legoData = require("./modules/legoSets");
 const path = require("path");
@@ -28,6 +30,27 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+
+app.use(clientSessions({
+  cookieName: "session", // this is the object name that will be added to 'req'
+  secret: "my secret is iron man is stronger than thor", // this should be a long un-guessable string.
+  duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
+  activeDuration: 1000 * 60 // the session will be extended by this many ms each request (1 minute)
+}));
+
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+}
+
 
 app.get('/', (req, res) => {
   res.render("home")
@@ -66,7 +89,7 @@ app.get("/lego/sets/:num", async (req,res)=>{
 
 
 
-app.get('/lego/addSet', (req, res) => {
+app.get('/lego/addSet', ensureLogin, (req, res) => {
   legoData.getAllThemes()
       .then((themes) => {
           res.render('addSet', { themes: themes });
@@ -76,7 +99,7 @@ app.get('/lego/addSet', (req, res) => {
       });
 });
 
-app.post('/lego/addSet', (req, res) => {
+app.post('/lego/addSet', ensureLogin, (req, res) => {
   const setData = req.body;
 
   legoData.addSet(setData)
@@ -88,7 +111,7 @@ app.post('/lego/addSet', (req, res) => {
       });
 });
 
-app.get('/lego/editSet/:num', async (req, res) => {
+app.get('/lego/editSet/:num', ensureLogin, async (req, res) => {
   try {
     // const setNum = req.params.num;
     let setData = await legoData.getSetByNum(req.params.num);
@@ -101,7 +124,7 @@ app.get('/lego/editSet/:num', async (req, res) => {
 });
 
 
-app.post('/lego/editSet', async (req, res) => {
+app.post('/lego/editSet', ensureLogin, async (req, res) => {
   try {
     const setNum = req.body.set_num;
     const setData = req.body;
@@ -114,7 +137,7 @@ app.post('/lego/editSet', async (req, res) => {
   }
 });
 
-app.get("/lego/deleteSet/:num", (req, res) => {
+app.get("/lego/deleteSet/:num", ensureLogin, (req, res) => {
   legoData.deleteSet(req.params.num).then(() => {
     res.redirect("/lego/sets");
   })
@@ -124,10 +147,69 @@ app.get("/lego/deleteSet/:num", (req, res) => {
 });
 
 
+// new get and post for login and logout functionality
+app.get("/login", function(req, res) {
+  res.render("login", { 
+    errorMessage: ""
+  });
+});
+
+app.get("/register", function(req, res) { 
+  res.render('register', { 
+    errorMessage: "",
+    successMessage: ""
+  });
+});
+
+app.post("/register", function(req, res) {
+  authData.registerUser(req.body)
+  .then(() => {
+      res.render('register', { successMessage: "User created!"})
+  })
+  .catch((err) => {
+      res.render('register', { errorMessage: err, userName: req.body.userName , successMessage: ""})
+  });
+});
+
+app.post("/login", function(req, res) {
+  req.body.userAgent = req.get('User-Agent');
+  authData.checkUser(req.body).then(function(user) {
+      req.session.user = {
+          userName: user.userName,
+          email: user.email,
+          loginHistory: user.loginHistory
+      }
+      res.redirect('/lego/sets');
+  })  
+  .catch(function(err) {
+      res.render('login', { errorMessage: err, userName: req.body.userName });
+  });
+});
+
+app.get("/logout", function(req, res) {
+  req.session.reset();
+  res.redirect('/');
+});
+
+app.get("/userHistory", ensureLogin, function (req, res) {
+  res.render('userHistory');
+}); 
+
+
+
 app.use((req, res, next) => {
   res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
 });
 
-legoData.initialize().then(()=>{
-  app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+// legoData.initialize().then(()=>{
+//   app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
+// });
+legoData.initialize()
+.then(authData.initialize)
+.then(function(){
+    app.listen(HTTP_PORT, function(){
+        console.log(`app listening on:  ${HTTP_PORT}`);
+    });
+}).catch(function(err){
+    console.log(`unable to start server: ${err}`);
 });
